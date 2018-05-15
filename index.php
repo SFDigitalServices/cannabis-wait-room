475336f7203df1545e6a1356ed7461a525c54b2c
Anthony Kong <anthony.kong@sfgov.org>
2018-05-15 10:38:01 -0700

    use same server time as client side to check for redirect

--- index.php
+++ index.php
@@ -11,10 +11,18 @@ date_default_timezone_set("US/Pacific");
 $launchDate = $config['launchDate'];
 $launchTime = strtotime($launchDate);
 
-$todayDate = date("F j, Y H:i:s");
-$todayTime = strtotime($todayDate);
+$serverDate = json_decode(file_get_contents("https://digitalservices.sfgov.org/servertime.php"));
+$serverHour = $serverDate->hours;
+$serverMinute = $serverDate->minutes;
+$serverSecond = $serverDate->seconds;
+$serverMonth = $serverDate->mon;
+$serverDay = $serverDate->mday;
+$serverYear = $serverDate->year;
 
-if($todayTime > $launchTime) {
+$theDate = date("F j, Y H:i:s", mktime($serverHour, $serverMinute, $serverSecond, $serverMonth, $serverDay, $serverYear));
+$theTime = strtotime($theDate);
+
+if($theTime > $launchTime) {
   header('Location:'.$config['redirect']);
 } else {
   include('wait.php');
