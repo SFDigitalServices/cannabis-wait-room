dea094525849edb383b04f6e14aed3cf398f70b5
Anthony Kong <anthony.kong@sfgov.org>
2018-05-15 07:36:24 -0700

    reload with timestamp

--- js/waiting.js
+++ js/waiting.js
@@ -5,24 +5,24 @@
       t: (new Date()).getTime()
     },
     success: function(resp) {
-      console.log(resp);
       var h = JSON.parse(resp);
       var u = (new Date(h.month + ' ' + h.mday + ', ' + h.year + ' ' + h.hours + ':' + h.minutes + ':' + h.seconds)).getTime();
       var i = (new Date(launchDate).getTime());
       if(u > i) {
-        location = location;
+        location.href = location.pathname + '?t=' + (new Date()).getTime();
       }
       setInterval(function() {
         u = moment(u).add(1, 'seconds').valueOf();
-        if(u > i) {
-          location = location;
-        }
         var diff = new moment.duration(i - u);
         var countdown = diff.days() + 'd ' + 
                         diff.hours() + 'h ' + 
                         diff.minutes() + 'm ' + 
                         (diff.seconds() <= 9 ? '0' + diff.seconds() : diff.seconds()) + 's';
-        $('#countdown').html(countdown);
+        if(u > i) {
+          location.href = location.pathname + '?t=' + (new Date()).getTime();
+        } else {
+          $('#countdown').html(countdown);
+        }
       }, 1000);
     }
   });
