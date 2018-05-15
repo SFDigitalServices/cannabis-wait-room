21899593ee9b83eed4efe8465d58901db7031741
Anthony Kong <anthony.kong@sfgov.org>
2018-05-15 14:23:34 -0700

    log exception and send 500 response

--- time.php
+++ time.php
@@ -3,7 +3,8 @@
   try {
       $data = file_get_contents("https://digitalservices.sfgov.org/servertime.php");
   } catch (Exception $ex) {
-      echo $ex;
+      error_log($ex->getMessage());
+      header("HTTP/1.1 500 Internal Server Error");
   }
   header('Content-Type: application/json');
   echo json_encode($data);
