07b29812170fbc8ecd8fe9445a945010cb9eb7d8
Anthony Kong <anthony.kong@sfgov.org>
2018-05-15 00:49:38 -0700

    remove comments

--- index.php
+++ index.php
@@ -5,21 +5,15 @@ header("Pragma: no-cache");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
-
 $config = include('config.php');
 date_default_timezone_set("US/Pacific");
 
 $launchDate = $config['launchDate'];
-// $launchDate = "May 15, 2018 8:10:00pm";
 $launchTime = strtotime($launchDate);
 
 $todayDate = date("F j, Y H:i:s");
 $todayTime = strtotime($todayDate);
 
-// echo '<div>today date time: ' . $today . '</div>';
-// echo '<div>launch date time: ' . date("F j, Y h:i:s", $launchTime) . '</div>';
-
-// echo $todayTime > $launchTime ? 'we are past launch' : 'we are not past launch';
 if($todayTime > $launchTime) {
   header('Location:'.$config['redirect']);
 } else {
